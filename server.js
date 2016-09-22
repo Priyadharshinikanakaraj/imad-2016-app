130206ac4442146a4a8ec16c84469f62c634d27f
Priyadharshinikanakaraj <kpriyadharshinik@gmail.com>
2016-09-22 20:29:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/sweets', function (req, res) {
-  res.send('Sweets will be served');
+  res.sendFile(path.join(__dirname, 'ui', 'sweets.html'));
 });
 app.get('/books', function (req, res) {
   res.send('Books will be given');

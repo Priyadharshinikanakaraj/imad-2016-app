d813843982279db9c678384d952852e2d05212fc
Priyadharshinikanakaraj <kpriyadharshinik@gmail.com>
2016-09-24 15:19:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 alert('My Javascripyt is running');
 var element = document.getElementById(text);
-element.innerHTML="New value being displayed";
+element.innerHTML='New value being displayed';
 var img = document.getElementById(madi);
 img.onclick=function(){
     img.style.marginLeft='100px';

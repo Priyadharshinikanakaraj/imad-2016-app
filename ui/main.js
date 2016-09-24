baaa8a960e01fac86fa689db1aa996982913467f
Priyadharshinikanakaraj <kpriyadharshinik@gmail.com>
2016-09-24 15:34:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ alert('My Javascripyt is running');
 var element = document.getElementById('text');
 element.innerHTML = 'New value being displayed';
 var img = document.getElementById('madi');
+function moveRight(){
+    marginLeft=marginLeft=10;
+    img.style.marginLeft = marginLeft+'px';
+}
 img.onclick=function(){
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft='100px';
 };
